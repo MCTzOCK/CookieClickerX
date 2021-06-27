b902dabd5f3ac5e7efb6e4b5dd17013e6fbee1e1
MCTzOCK <dev@mctzock.de>
2021-06-27 11:18:19 +0200

    Added some more stuff

--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@
  * @copyright 2018-2021 Ben Siebert. All rights reserved.
  */
 
+let allowClickAnimation = true
+let cookies = 0
+let grandmas = 0
 
+window.onload = function () {
+    document.getElementById('cookie').addEventListener('click', (event) => {
+        cookies++;
+        if (allowClickAnimation) {
+            document.getElementById('cookie').classList.add('animationShrink')
+            allowClickAnimation = false
+            setTimeout(() => {
+                document.getElementById('cookie').classList.remove('animationShrink')
+                allowClickAnimation = true
+            }, 500)
+        }
+    });
+    setInterval(() => {
+        document.getElementById('cookieDisplay').innerText = cookies.toString()
+        document.getElementById('grandmaDisplay').innerText = grandmas.toString()
+    }, 50);
+}
